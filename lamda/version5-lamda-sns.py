6e593654c7df479d462aa9fd07e76045f7a0e46d
FindingNitish <jaishiishwor11@gmail.com>
2026-09-20 18:28:02 +0900

    Update version5-lamda-sns.py

--- lamda/version5-lamda-sns.py
+++ lamda/version5-lamda-sns.py
@@ -1,1 +1,42 @@
+import boto3
 
+dynamodb = boto3.resource('dynamodb')
+sns = boto3.client('sns')
+
+TOPIC_ARN = "arn:aws:sns:ap-northeast-3:932333195607:FloodAlertTopic"
+
+def lambda_handler(event, context):
+
+    table = dynamodb.Table('RiverLevels')
+
+    response = table.get_item(
+        Key={
+            'RiverName': 'Bheri'
+        }
+    )
+
+    item = response['Item']
+
+    RiverName = item['RiverName']
+    WaterLevel = int(item['WaterLevel'])
+
+    if WaterLevel >= 9:
+
+        Status = "Danger"
+
+        sns.publish(
+            TopicArn=TOPIC_ARN,
+            Subject='Flood Alert',
+            Message=f'Flood Alert!\n\nRiver: {RiverName}\nWater Level: {WaterLevel}m\nStatus: Danger'
+        )
+
+    elif WaterLevel >= 7:
+        Status = "Warning"
+
+    else:
+        Status = "Safe"
+
+    return {
+        "statusCode": 200,
+        "body": f"River: {RiverName} | Level: {WaterLevel}m | Status: {Status}"
+    }
